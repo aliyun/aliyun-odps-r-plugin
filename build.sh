653cbc6b82b715362d31639f710cb74d1696fab6
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2023-10-26 11:19:17 +0800

    Cleanse building script

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
-
-source ./config.sh
+#! /bin/bash
 basepath=$(cd "$(dirname "$0")";pwd)
+source $basepath/config.sh
+
+mkdir -p $basepath/build > /dev/null
+rm -rf $basepath/build/*
+
+# Generate documentation
+echo "Generating documentation..."
+sh -x $basepath/gendoc.sh
+
 javasrc=$basepath/java
 libpath=$basepath/RODPS/inst/lib
 RPackageName="RODPS_${RVERSION}.tar.gz"
 ThirdLibPath=$basepath/java/target/lib
 
-echo "Start build R package in $basepath at $d"
+echo "Start build R package in $basepath at $d..."
 
 # mvn package
 cd $javasrc
@@ -29,3 +36,4 @@ mkdir -p target
 mv $RPackageName target/
 
 echo "Generate $basepath/target/$RPackageName"
+echo "Success"
